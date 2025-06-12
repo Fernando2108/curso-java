6934f6cf99ccf3a8f1a77169802de8aeed0a7a92
Fernando <fernandoregidor21@gmail.com>
2025-06-12 13:53:12 +0200

    subir

--- 10-operators.java/shop.java
+++ 10-operators.java/shop.java
@@ -1,5 +1,56 @@
-public class shop
+import java.util.Scanner;
+
+public class shop {
+
+    static final double PACKET_PRICE = 20;
  
-static boolean checkAge(int age) {
-        System.out.println("Ropa en paquetes" + age);
-        return age >25 ? true 
+    static int getDataByScanner() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.printf("Vendemos paquetes de pantalones y camisetas a %s EU" , PACKET_PRICE);
+    System.out.println("Dime el numero de paquetes de pantalones");
+    int packets = scanner.nextInt();
+    System.out.println("");
+    scanner.close();
+    return packets; 
+    } 
+    
+    public static void main(String[] args) {
+       int numPackets = getDataByScanner();
+       System.out.println(numPackets);
+       double discount = 0.15; 
+       int shippingByPacket = 0;
+
+       if (numPackets < 5) {
+           System.out.println("Numero de paquetes insuficiente");
+            return;
+
+       } else if (numPackets < 10) {
+          discount = 0.05;
+          shippingByPacket = 10 ;
+
+       } else if (numPackets < 20) {
+          discount = 0.1;
+          shippingByPacket = 5;
+       }
+
+       // Validos para los casos 2, 3 y 4
+
+        double total = numPackets * PACKET_PRICE; // 100
+        double finalDiscount = total * discount; // 10% -> 10
+        double totalShipping = numPackets * shippingByPacket;
+        double finalPrice = total - finalDiscount + totalShipping;
+
+        System.out.println("""
+                Numero de paquetes         %d
+                Precio total del producto  %.2f EU
+                Descuento aplicado         %.2f EU
+                Gastos de envió            %.2f EU
+                ----------------------------------
+                TOTAL                     %.2f EU
+                """.formatted(numPackets, total, finalDiscount, totalShipping, finalPrice));
+                
+       
+             
+    }
+
+}            
---
+++ 11-random/Roll.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scaner;
+
+public class Roll {
+
+    private static int getDataByScaner () {
+
+        private static void guessRoll (int random) {
+            int userNumber = getDataByScanner
+        }
+
+    }
+}
