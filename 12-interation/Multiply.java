57c3614ff26893aaf7324a4d0fe2c90a80eec878
Fernando <fernandoregidor21@gmail.com>
2025-06-13 12:14:37 +0200

    subir

--- 12-interation/Multiply.java
+++ 12-interation/Multiply.java
@@ -1,10 +1,14 @@
 public class Multiply {
 
-    public static ind LIMIT = 10;
+    public static int LIMIT = 10;
+
+    public static void main(String[] args) {
+        
+    }
 
     public static void showTable(int number) {
         System.out.println("tabla de %s\n", number);
-        for (int i =o; i < LIMIT; i++){
+        for (int i = 0; i < = 10; i++){
             System.out.println("%s x %s\n = ");
         }
     }
