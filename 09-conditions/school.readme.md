6124f7d1b6ec92ce0718c726e2e88f7ff78ab3d7
Fernando <fernandoregidor21@gmail.com>
2025-06-13 11:27:21 +0200

    Add for samples and info

---
+++ 09-conditions/school.readme.md
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+Public clas school.readme.md {
+
+
+
+    static int getAgeByScanner() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Dime tu edad");
+        int age = scanner.nextInt();
+        System.out.println("");
+        scanner.close();
+        return age;
+
+}
+          
+    public static void main(String[] args) {
+
+      String[] mensajes = {
+        "Kinder (de 6 años inclusive): Lunes y Miércoles de 16 a 17 1stYear"
+        
+
+      }
+
+
+    }
---
+++ 11-interation/BucleFor.java
@@ -0,0 +1,58 @@
+
+
+public class BucleFor {
+
+    final int LIMIT = 10;
+
+    public static void showInfo () {
+        System.out.println("Soy showInfo" + );
+    }
+
+public static void main(String[] args) {
+for (int i = 0; i < 10; i++) {
+    System.out.println("vuelta numero" + round);
+     }
+ 
+
+public static void arrayFor() {
+   String[] naames - {"Pepe", "Ernestina", "Sofia", "Juan"};
+
+   for (int i = 0; i < names.length; i++) {
+     System.out.println("Hola" + names[i]);
+   }
+
+}
+
+public static void arrayForEach() {
+
+    for (String name : NAMES) {
+        System.out.println("Hola" + name);
+        
+    }
+}
+
+public static void iterateString() {
+
+    String sample = "Lo que os de la real gana";
+    for (int index = 0; index < sample.length(); i++) {
+    System.out.println(sample.charAt(i));
+    }
+}
+
+public static void iterateString() {
+
+    for 
+
+}
+        
+   
+
+public static void main(strig[] args) {
+
+}
+
+    System.out.println(i);
+        
+}
+    
+}
